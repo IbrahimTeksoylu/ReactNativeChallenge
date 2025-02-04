c4a5b80b6091943639c36257f1fc2dfa19d35aa2
ibrahim <teksoyluibrahim@gmail.com>
2025-02-04 22:38:36 +0000

    feat(HomeScreen): integrate Card component to display data in a scrollable view

--- screens/Home/HomeScreen.tsx
+++ screens/Home/HomeScreen.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, ScrollView, View } from "react-native";
+import { Card } from "../../components/Card/Card";
 
 export const HomeScreen = () => {
+  
+  const data = [
+    { from: "2018-01-20T08:00Z", to: "2018-01-20T08:30Z", intensity: { forecast: 45, actual: 70, index: "very-low" } },
+    { from: "2018-01-20T10:00Z", to: "2018-01-20T10:30Z", intensity: { forecast: 80, actual: 90, index: "low" } },
+    { from: "2018-01-20T12:00Z", to: "2018-01-20T12:30Z", intensity: { forecast: 120, actual: 122, index: "moderate" } },
+    { from: "2018-01-20T13:30Z", to: "2018-01-20T14:00Z", intensity: { forecast: 200, actual: 195, index: "high" } },
+    { from: "2018-01-20T17:30Z", to: "2018-01-20T18:00Z", intensity: { forecast: 280, actual: 265, index: "very-high" } },
+  ];
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      {data.map((d, index) => (
+        <Card key={index} from={d.from} to={d.to} intensity={d.intensity} />
+      ))}
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    padding: 16,
+  },
+});
